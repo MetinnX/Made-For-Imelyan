ac0d53fe478db1cd57ed6a08713e945229e8687f
MetinnX <aditya.utin4@gmail.com>
2026-07-07 15:14:18 +0800

    Update CollageScene.jsx

--- src/components/CollageScene.jsx
+++ src/components/CollageScene.jsx
@@ -3,17 +3,20 @@ import React from 'react';
 export default function CollageScene() {
   return (
     <div className="scene-container center-content flex-col fade-in-scene">
-      <h1 className="epic-title">I ❤️ YOU</h1>
+      {/* Container formasi dibuat pas di tengah */}
       <div className="heart-formation-grid">
-        {/* Melakukan pengulangan otomatis untuk merender 17 bingkai foto polaroid */}
+        {/* Loop otomatis untuk 17 foto polaroid */}
         {[...Array(17)].map((_, idx) => (
           <div 
             key={idx} 
             className={`scatter-polaroid target-pos-${idx + 1}`}
-            style={{ animationDelay: `${0.3 + (idx * 0.25)}s` }} // Memberikan jeda muncul beruntun
+            style={{ 
+              animationDelay: `${0.2 + (idx * 0.15)}s`, // Animasi sedikit dipercepat agar sebaran terasa mulus
+            }} 
           >
-            {/* Foto dipanggil berurutan dari love-1.jpg sampai love-17.jpg */}
-            <img src={`/assets/images/love-${idx + 1}.jpg`} alt={`Memory ${idx + 1}`} />
+            <div className="polaroid-frame">
+              <img src={`/assets/images/love-${idx + 1}.jpg`} alt={`Memory ${idx + 1}`} />
+            </div>
           </div>
         ))}
       </div>
