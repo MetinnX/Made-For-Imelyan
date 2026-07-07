7f7e344fd513eb18692cfb6ba04343439cfe2d2e
MetinnX <aditya.utin4@gmail.com>
2026-07-07 12:57:09 +0800

    Create texts.jsx

---
+++ src/data/texts.jsx
@@ -0,0 +1,21 @@
+// src/data/texts.js
+export const bookPages = [
+  {
+    id: 0,
+    title: "Happy Birthday Imelyan! ❤️",
+    body: "Sentuh atau geser buku untuk mulai membaca.",
+    image: "/assets/images/cover-buku.jpg"
+  },
+  {
+    id: 1,
+    title: "Dear Imelyan..",
+    body: "Selamat ulang tahun! Di hari yang sangat spesial ini, aku sengaja meluangkan waktu membuatkan website kejutan ini khusus untukmu.",
+    image: "/assets/images/pasangan-kiri-1.jpg"
+  },
+  {
+    id: 2,
+    title: "Harapanku...",
+    body: "Semoga di usia yang baru ini kebahagiaan selalu menyertai setiap langkahmu. As long as you're here.",
+    image: "/assets/images/pasangan-kanan-1.jpg"
+  }
+];
